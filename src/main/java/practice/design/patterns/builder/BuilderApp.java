a5959cdf3d7497d1d76fd4258afd9d8072932c1f
Muhammad Usman <hafizusman530@ymail.com>
2019-01-14 17:29:09 +0500

    Update BuilderApp.java

--- src/main/java/practice/design/patterns/builder/BuilderApp.java
+++ src/main/java/practice/design/patterns/builder/BuilderApp.java
@@ -5,9 +5,9 @@ import practice.constants.SEX;
 
 public class BuilderApp {
     public static void main(String[] args) {
-        PersonPerforma personPerforma1 = new PersonPerforma.PersonBuilder("usman").build();
-        PersonPerforma personPerforma2 = new PersonPerforma.PersonBuilder("usman")
-                .withAge("26")
+        PersonPerforma personPerforma1 = new PersonPerforma.PersonBuilder("Dummy").build();
+        PersonPerforma personPerforma2 = new PersonPerforma.PersonBuilder("Dummy 2")
+                .withAge("134")
                 .withMartialStatus(MartialStatus.MARRIED)
                 .withSex(SEX.MALE)
                 .build();
